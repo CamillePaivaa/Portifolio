1c64aacc361e95d188ea7cfb05fd7740d50c76bb
Camille Paiva <camille.paiva@ufv.br>
2023-05-02 18:36:47 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".swiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    keyboard: true,
+  });
+
+  $("#fileRequest").click(function() {
+    // // hope the server sets Content-Disposition: attachment!
+    window.location = 'Profile.pdf';
+});
